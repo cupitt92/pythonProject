4deb33850e6f32899df06a985244a3da60581df9
cupitt92 <poke_and@hotmail.co.uk>
2017-07-11 19:16:18 +0100

    Added python code examples

---
+++ finalProject.py
@@ -0,0 +1,17 @@
+>>> 
+def main():
+    print("""
+        Welcome to Grade Central
+
+	[1] - Enter Grades
+	[2] - Remove Student
+	[3] - Student Average Grades
+	[4] - Exit
+    """)
+    action = input('What would you like to do today? (Enter a number)')
+			       
+SyntaxError: unexpected indent
+>>> 
+>>> main()
+
+
